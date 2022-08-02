ef945a911a8619502b6a620532af2e643137e6ea
LAripping <leo.tsaou@gmail.com>
2022-08-02 21:19:31 +0300

    Port http-for-headers to Python3

--- http-for-headers.py
+++ http-for-headers.py
@@ -1,27 +1,23 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 
 #Original Version From https://gist.github.com/phrawzty/62540f146ee5e74ea1ab
 
-import SimpleHTTPServer
-import SocketServer
-import logging
+import http.server
+import socketserver
 import sys
 
 PORT = int(8000 if len(sys.argv)==1 else sys.argv[1])
 
-class GetHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+class MyHandler(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
-        print('\n[*] '+str(self.command)+' '+str(self.path))
-        print('\n[*] '+str(self.headers))
-        SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
-    def do_PUT(self):
-        logging.info('\n'+str(self.command)+' '+str(self.path))
-        logging.info('\n'+str(self.headers))
-        SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
-        
+        print('\n'+str(self.command)+' '+str(self.path))
+        print(str(self.headers))
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
 
-Handler = GetHandler
-httpd = SocketServer.TCPServer(("", PORT), Handler)
+Handler = MyHandler
+httpd = socketserver.TCPServer(("", PORT), Handler)
 
 print("Listening on port %d..." % PORT)
 httpd.serve_forever()
