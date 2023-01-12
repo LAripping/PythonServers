9fd69bc9410e22e091dd263c40e36bd44104ddaf
Leo Tsaousis <leo.tsaou@gmail.com>
2023-01-12 12:22:38 +0000

    Update https.py

--- https.py
+++ https.py
@@ -1,15 +1,16 @@
-from http.server import HTTPServer, BaseHTTPRequestHandler
+from http.server import HTTPServer, BaseHTTPRequestHandler, SimpleHTTPRequestHandler
 import ssl
 import sys
 
-# Usage siptls-server.py listenIP port path/to/key.pem path/to/cert.pem
+if len(sys.argv)<4:
+  print("Usage https.py listenIP port path/to/key.pem path/to/cert.pem")
 
 listenip = sys.argv[1]
 port = int(sys.argv[2])
 keyfile = sys.argv[3]
 certfile = sys.argv[4]
 
-httpd = HTTPServer((listenip,port), BaseHTTPRequestHandler)
+httpd = HTTPServer((listenip,port), SimpleHTTPRequestHandler)
 httpd.socket = ssl.wrap_socket(httpd.socket, keyfile=keyfile, certfile=certfile, server_side=True)
 httpd.serve_forever()
 print("Serving on "+port)
